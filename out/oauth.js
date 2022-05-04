4a52ef6f4461046731c6e9e67dc805bd6f963514
farrar142 <gksdjf1690@gmail.com>
2022-05-05 00:09:52 +0900

    jenkins deploy

--- out/oauth.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-  console.log("wind loaded");
-  document.querySelector("button").addEventListener("click", function () {
-    chrome.identity.getAuthToken({ interactive: true }, function (token) {
-      console.log(token);
-    });
-  });
-};
-console.log("test");
