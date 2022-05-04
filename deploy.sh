20e1b3ad368ec3086453c98d6665e9c0a21a50a0
farrar142 <gksdjf1690@gmail.com>
2022-05-04 22:12:31 +0900

    jenkins deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 # docker-compose  --force-rm -f docker-compose-deploy.yml up --build -d
-docker-compose -f docker-compose-deploy.yml --force-rm
-docker-compose -f docker-compose-deploy.yml -up -d
+docker-compose -f docker-compose-deploy.yml build --force-rm
+docker-compose -f docker-compose-deploy.yml up -d
